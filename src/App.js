deca9a9c9285f52b9e36c4647cc736c47fe1526d
Bekarys <bekarys20050812@gmail.com>
2024-10-19 19:29:44 +0500

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          min={0}
+          max={9}
+          value={step}
+          onChange={(e) => setStep(e.target.value)}
+        ></input>
+        <p>{step}</p>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - +step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(+e.target.value)}
+        ></input>
+        <button onClick={() => setCount((c) => c + +step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+
+        <span>{date.toDateString()}</span>
+
+        <div>
+          <button
+            onClick={() => {
+              setStep(0);
+              setCount(0);
+            }}
+          >
+            Reset
+          </button>
+        </div>
+        {console.log(date)}
+      </p>
+    </div>
+  );
+}
